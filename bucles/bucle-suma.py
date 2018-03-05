9f4bfff6ad287a6341a937f2571f5a76ff4f4058
Carlosleonolivares <36187897+Carlosleonolivares@users.noreply.github.com>
2018-03-05 12:08:04 +0100

    Add files via upload

---
+++ bucles/bucle-suma.py
@@ -0,0 +1,21 @@
+#coding: utf8
+
+salir="N"
+valor1=1
+valor2=valor1+1
+valor3=valor1+2
+valor4=valor1+3
+valor5=valor1+4
+total=valor1+valor2+valor3+valor4+valor5
+while(salir=="N"):
+	print valor1,"+",valor2,"+",valor3,"+",valor4,"+",valor5,"=",total
+
+	
+	salir = "Y"
+	
+    
+	
+    
+    
+
+
