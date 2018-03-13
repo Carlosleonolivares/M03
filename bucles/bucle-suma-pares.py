9435adc06dc6a89af00a92bf7e8390d670505dd5
Carlosleonolivares <36187897+Carlosleonolivares@users.noreply.github.com>
2018-03-13 13:14:34 +0100

    Create bucle-suma-pares.py

---
+++ bucles/bucle-suma-pares.py
@@ -0,0 +1,21 @@
+# coding: utf8
+# Carlos león olivares
+# 12/03/2018
+
+import os
+os.system("clear")
+
+salir = "N"
+valor=1
+suma=0
+while ( salir=="N" ):
+	
+	print (numero)
+
+
+	suma=suma+valor
+	valor=valor+1
+	
+	if (valor>5):
+    	    salir = "S"
+print (suma)
